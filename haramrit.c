0623042df4a4229a07c466d131ec00dabeaa575f
haramrit09k <haramrit09k@gmail.com>
2017-03-25 11:53:20 +0530

    Added testing functions in haramrit.c

--- haramrit.c
+++ haramrit.c
@@ -1,7 +1,112 @@
+//**********ARRAY_MIN_INT**********
+
+//#include<stdio.h>
+//#include"array.h"
+//
+//int main()
+//{
+//	int n, i;
+//	
+//	printf("Enter the size of the array: ");
+//	scanf("%d",&n);
+//	
+//	int a[n];
+//	
+//	printf("Enter the elements: \n");
+//	
+//	for(i= 0; i< n; i++)
+//	scanf("%d",&a[i]);
+//	
+//	int x= array_min_int(a, n);
+//	
+//	printf("array min : %d",x);
+//	
+//	return 0;
+//	
+//}
+
+
+//**********ARRAY_MAX_INT**********
+
+//#include<stdio.h>
+//#include"array.h"
+//
+//int main()
+//{
+//	int n, i;
+//	
+//	printf("Enter the size of the array: ");
+//	scanf("%d",&n);
+//	
+//	int a[n];
+//	
+//	printf("Enter the elements: \n");
+//	
+//	for(i= 0; i< n; i++)
+//	scanf("%d",&a[i]);
+//	
+//	int x= array_max_int(a, n);
+//	
+//	printf("array max : %d",x);
+//	
+//	return 0;
+//	
+//}
+
+
+//**********ARRAY_MIN_FLOAT**********
+
+//#include<stdio.h>
+//#include"array.h"
+//
+//int main()
+//{
+//	int n, i;
+//	
+//	printf("Enter the size of the array: ");
+//	scanf("%d",&n);
+//	
+//	float a[n];
+//	
+//	printf("Enter the elements: \n");
+//	
+//	for(i= 0; i< n; i++)
+//	scanf("%f",&a[i]);
+//	
+//	float x= array_min_float(a, n);
+//	
+//	printf("array min : %f",x);
+//	
+//	return 0;
+//	
+//}
+
+
+//**********ARRAY_MAX_FLOAT**********
+
 #include<stdio.h>
+#include"array.h"
 
 int main()
 {
+	int n, i;
+	
+	printf("Enter the size of the array: ");
+	scanf("%d",&n);
+	
+	float a[n];
+	
+	printf("Enter the elements: \n");
+	
+	for(i= 0; i< n; i++)
+	scanf("%f",&a[i]);
+	
+	float x= array_max_float(a, n);
+	
+	printf("array max : %f",x);
 	
 	return 0;
+	
 }
+
+
