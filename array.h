da6aa2a531dc4f1ec7d6c3693dd7d956c414d096
Srijan-Ramavat <srijan.ramavat@gmail.com>
2017-03-25 16:45:29 +0530

    Added multiplication and subtract function

--- array.h
+++ array.h
@@ -1,12 +1,10 @@
 //Header file for array related operations (one dimensional arrays(lists) and two dimensional arrays(matrices))
-
 #include<stdio.h>
 #include<stdlib.h>
-
 //===================================
 int r,c;
 //===============================
-void prt_mat(int m,int n,float mat[m][n])
+void prt_mat(int m,int n,int (*mat)[n])
 {
   int i,j;
   r=m;
@@ -15,12 +13,12 @@ void prt_mat(int m,int n,float mat[m][n])
   {
     for(j=0;j<n;j++)
     {
-      printf("\n Matrix[%d][%d]=%f\n",i+1,j+1,mat[m][n]);
+      printf("\n Matrix[%d][%d]=%d\n",i+1,j+1,mat[m][n]);
     }
   }
 }
-//===================================================
-void add_mat(int m,int n,int p,int q,float add[m][n],float a[m][n],float b[p][q])
+//==========================================================================
+void add_mat(int m,int n,int p,int q,int (*add)[n],int (*a)[n],int (*b)[q])
 {
   int i,j;
   if(p==m && q==n)
@@ -35,10 +33,62 @@ void add_mat(int m,int n,int p,int q,float add[m][n],float a[m][n],float b[p][q]
   }
   else
   {
-    printf("Can't Added!!!\n");
+    printf("Error : Can't Add! now of rows and columns are not equal in matrices\n");
+    exit(0);
   }
 }
-//================================================
+//========================================================================
+void sub_mat(int m,int n,int p,int q,int (*sub)[n],int (*a)[n],int (*b)[q])
+{
+  int i,j;
+  if(p==m && q==n)
+  {
+      for(i=0;i<m;i++)
+      {
+        for(j=0;j<n;j++)
+        {
+          sub[i][j]=a[i][j]-b[i][j];
+        }
+      }
+  }
+  else
+  {
+    printf("Error : Can't subtract! now of rows and columns are not equal in matrices\n");
+    exit(0);
+  }
+}
+//========================================================================
+/*Matrix MULTIPLICATION */
+void mul_mat(int m,int n,int p,int q,int (*mul)[q],int (*a)[n],int (*b)[q])
+{
+    int i,j,k;
+    if(n==p)
+    {
+      for(i=0;i<m;i++)
+      {
+        for(j=0;j<q;j++)
+        {
+          mul[i][j]=0;
+        }
+      }
+      for(i=0;i<m;i++)
+      {
+        for(j=0;j<q;j++)
+        {
+          for (k=0;k<p;k++)
+          {
+             mul[i][j]=mul[i][j]+(a[i][k]*b[k][j]);
+          }
+        }
+      }
+    }
+    else
+    {
+      printf("Error : Can't Multiply! number of columns in 1st matrix is not equal to number of rows in the 2nd matrix.\n ");
+      exit(0);
+    }
+}
+//========================================================================
 //Finding minimum element in an array of integer values
 int array_min_int(int a[], int n) {
   int c, min, index;
@@ -55,8 +105,7 @@ int array_min_int(int a[], int n) {
 
   return index;
 }
-
-
+//=======================================================================
 //Finding minimum element in an array of float values
 float array_min_float(float a[], int n) {
   int c, index;
@@ -113,63 +162,61 @@ float array_max_float(float a[], int n) {
 
 //Sorting elements in the array in ascending order
  void array_asc_sort_int(int a[], int n)
-   {                                                           
-       int i, j, t;                                            
-                                                               
-       for(i = 1; i <= n-1; i++)                               
-          for(j = 1; j <= n-i; j++)                             
-             if(a[j-1] >= a[j])                                
+   {
+       int i, j, t;
+
+       for(i = 1; i <= n-1; i++)
+          for(j = 1; j <= n-i; j++)
+             if(a[j-1] >= a[j])
              {
-                t = a[j-1];                                    
-                a[j-1] = a[j];                                 
-                a[j] = t;                                      
-             }                                                 
-   }           
+                t = a[j-1];
+                a[j-1] = a[j];
+                a[j] = t;
+             }
+   }
 //Sorting elements in the array in ascending order
  void array_asc_sort_float(float a[], int n)
-   {                                                           
-       int i, j, t;                                            
-                                                               
-       for(i = 1; i <= n-1; i++)                               
-          for(j = 1; j <= n-i; j++)                             
-             if(a[j-1] >= a[j])                                
+   {
+       int i, j, t;
+
+       for(i = 1; i <= n-1; i++)
+          for(j = 1; j <= n-i; j++)
+             if(a[j-1] >= a[j])
              {
-                t = a[j-1];                                    
-                a[j-1] = a[j];                                 
-                a[j] = t;                                      
-             }                                                 
-   }           
+                t = a[j-1];
+                a[j-1] = a[j];
+                a[j] = t;
+             }
+   }
 
 
 //Sorting elements in the array in descending order
  void array_desc_sort_int(int a[], int n)
-   {                                                           
-       int i, j, t;                                            
-                                                               
-       for(i = 1; i <= n-1; i++)                               
-          for(j = 1; j <= n-i; j++)                             
-             if(a[j-1] <= a[j])                                
+   {
+       int i, j, t;
+
+       for(i = 1; i <= n-1; i++)
+          for(j = 1; j <= n-i; j++)
+             if(a[j-1] <= a[j])
              {
-                t = a[j-1];                                    
-                a[j-1] = a[j];                                 
-                a[j] = t;                                      
-             }                                                 
-   }           
+                t = a[j-1];
+                a[j-1] = a[j];
+                a[j] = t;
+             }
+   }
 
 //Sorting float elements in the array in descending order
  void array_desc_sort_float(float a[], int n)
-   {                                                           
+   {
        int i, j;
-       float t;                                            
-                                                               
-       for(i = 1; i <= n-1; i++)                               
-          for(j = 1; j <= n-i; j++)                             
-             if(a[j-1] <= a[j])                                
+       float t;
+
+       for(i = 1; i <= n-1; i++)
+          for(j = 1; j <= n-i; j++)
+             if(a[j-1] <= a[j])
              {
-                t = a[j-1];                                    
-                a[j-1] = a[j];                                 
-                a[j] = t;                                      
-             }                                                 
+                t = a[j-1];
+                a[j-1] = a[j];
+                a[j] = t;
+             }
    }
-   
-   
