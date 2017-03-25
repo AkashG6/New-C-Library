068a2e876ab2bf4d39081e2c23df55d47aceb2d9
Srijan-Ramavat <srijan.ramavat@gmail.com>
2017-03-25 12:03:17 +0530

    rmg

--- srijan.c
+++ srijan.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 #include"array.h"
-int main(int argc, char const *argv[]) {
-  
+int main(int argc, char const *argv[])
+{
+  int m,n;
+  float a[m][n];
+  printf("\nEnter the row and column :" );
+  scanf("%d%d",&m,&n );
+  read_mat(m,n,a);
+  prt_mat(m,n,a);
+
   return 0;
 }
