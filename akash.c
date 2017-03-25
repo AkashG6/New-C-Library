1456e13b9b0c0136045a0e5d39be770425db3c8a
AkashG6 <akashgarule@gmail.com>
2017-03-25 11:16:40 +0530

    added function dec_to_binary

--- akash.c
+++ akash.c
@@ -5,32 +5,51 @@
 
 int main()
 {
-															// ones complement
-	int i,n;
-	printf("Enter size\n");
+	// 														// ones complement
+	// int i,n;
+	// printf("Enter size\n");
+	// scanf("%d",&n);
+	// int array[n],a[n];
+
+	// printf("enter no.\n");
+	// for (i = 0; i < n; i++)
+	// {
+	// 	scanf("%d",&array[i]);
+	// }
+
+	// a[n] = *ones_complement(a,array,n);
+
+
+	// // for (i = 0; i < n; i++)
+	// // {
+	// // 	printf("%d",a[i]);
+	// // }
+	// // printf("\n");
+	// // printf("The no. was\n");
+	// // for (i = 0; i < n; i++)
+	// // {
+	// // 	printf("%d",array[i]);
+	// // }
+
+
+															// dec to bin
+
+	int n,i,size;
+	printf("Enter a number\n");
 	scanf("%d",&n);
-	int array[n],a[n];
-
-	printf("enter no.\n");
-	for (i = 0; i < n; i++)
+	printf("Enter number of bits\n");
+	scanf("%d",&size);
+	int array[size];
+	array[size] = *dec_to_bin(array,size,n);
+	for (i = 0; i < size; i++)
 	{
-		scanf("%d",&array[i]);
+		printf("%d ",array[i] );
 	}
+	printf("\n");
+	printf("%d\n",n );
 
-	a[n] = *ones_complement(a,array,n);
 
-
-	// for (i = 0; i < n; i++)
-	// {
-	// 	printf("%d",a[i]);
-	// }
-	// printf("\n");
-	// printf("The no. was\n");
-	// for (i = 0; i < n; i++)
-	// {
-	// 	printf("%d",array[i]);
-	// }
-
+															// 
 
 }
 
